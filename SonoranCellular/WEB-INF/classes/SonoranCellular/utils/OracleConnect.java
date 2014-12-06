1d7ed10551b135b6738f9300ee7507bb8607c5da
Zuoming Shi <zuomingshi@oxford.(none)>
2014-12-06 13:15:04 -0700

    test2

--- SonoranCellular/WEB-INF/classes/SonoranCellular/utils/OracleConnect.java
+++ SonoranCellular/WEB-INF/classes/SonoranCellular/utils/OracleConnect.java
@@ -5,6 +5,8 @@ public class OracleConnect
     public static final String connect_string = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
     public static final String user_name = "zuomingshi@oracle.aloe";
     public static final String password = "a9335";
+    //public static final String user_name = "zuomingshi@oracle.aloe";
+    //public static final String password = "a9335";
 }
 
 
