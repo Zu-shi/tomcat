967aa72073a826f5eb6d93b20cf9d0242bdad64a
Zuoming Shi <zuomingshi@oxford.(none)>
2014-12-10 13:47:11 -0700

    commit loginServlet before pull

--- SonoranCellular/WEB-INF/classes/SonoranCellular/servlets/LoginServlet.java
+++ SonoranCellular/WEB-INF/classes/SonoranCellular/servlets/LoginServlet.java
@@ -15,7 +15,7 @@ public class LoginServlet extends HttpServlet
     {
         super();
     }
-
+    
     private OracleConnect oc = new OracleConnect();
     private Statement s;
     private Connection c;
@@ -31,13 +31,13 @@ public class LoginServlet extends HttpServlet
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
 			c = DriverManager.getConnection(connect_string,user_name,password);
-		
+            
 			if (c == null) throw new Exception("Connection to database failed.");
-
+            
 			s = c.createStatement();
 			
 			if (s == null) throw new Exception("Statement creation fialed.");
-		
+            
 		} catch (Exception e) {
 			e.printStackTrace();
 			return;
@@ -48,8 +48,8 @@ public class LoginServlet extends HttpServlet
     
     public void destroy() {
     	try {
-		c.commit();
-		c.close();
+            c.commit();
+            c.close();
     	} catch (Exception e) {
     		e.printStackTrace();
     	}
@@ -61,19 +61,19 @@ public class LoginServlet extends HttpServlet
         out.println("<head>");
         out.println("<title>SonoranCellular logged in</title>");
         out.println("</head>");
-
+        
         out.println("<body>");
-
-
+        
+        
         out.println("<p>");
         out.println("<center>");
         out.println("<font size=7 face=\"Arial, Helvetica, sans-serif\" color=\"#000066\">");
         out.println("<center>\n<strong>SonoranCellular</strong></br>");
         out.println("</center>\n<hr color=\"#000066\">");
         out.println("<br><br>");
-
+        
     }
-
+    
     public void drawFooter(HttpServletRequest req, PrintWriter out)
     {
         out.println("</center>");
@@ -81,30 +81,23 @@ public class LoginServlet extends HttpServlet
         out.println("</body>");
         out.println("</html>");
     }
-
-
+    
+    
     private void drawActiveOptions(HttpServletRequest req, PrintWriter out)
     {
-        System.out.println("HW8: drawActiveOptions");
-
+        
         out.println("<br>");
-
+        
         out.println("<form name=\"AddPlan\" action=AddPlan method=get>");
         out.println("<input type=submit name=\"AddPlan\" value=\"Add a Plan\">");
         out.println("</form>");
         
         out.println("<br>");
-
+        
         out.println("<form name=\"FindBill\" action=FindBill method=get>");
         out.println("<input type=submit name=\"FindBill\" value=\"Print Bill for a billing period\">");
         out.println("</form>");
-
-        out.println("<br>");
-
-        out.println("<form name=\"PlanShare\" action=./JSP/SharedAssignment.jsp>");
-        out.println("<input type=submit name=\"SharedAssignment\" value=\"Who is assigned to the same plan?\">");
-        out.println("</form>");
-
+        
         out.println("<br>");
         
         out.println("<form name=\"PlanShare\" action=./JSP/SharedAssignment.jsp>");
@@ -113,70 +106,49 @@ public class LoginServlet extends HttpServlet
         
         out.println("<br>");
         
-        out.println("<form name=\"AddMaster\" action=AddMaster method=get>");
-        out.println("<input type=submit name=\"AddMaster\" value=\"Set master account for this account.\">");
-        out.println("</form>");
-        
-        out.println("<br>");
-        
-        out.println("<form name=\"ViewDependents\" action=ViewDependents method=get>");
-        out.println("<input type=submit name=\"ViewDependents\" value=\"Show list of accounts dependent on this account.\">");
+        out.println("<form name=\"LogoutServlet\" action=LogoutServlet method=get>");
+        out.println("<input type=submit name=\"LogoutServlet\" value=\"Log out\">");
         out.println("</form>");
     }
-
+    
     private void drawFailOptionsNoAccountNumber(HttpServletRequest req, PrintWriter out)
     {
         out.println("<font size=5 face=\"Arial,Helvetica\">");
         out.println("<b>Error: account number does not exist.</b></br>");
-
+        
         out.println("<hr");
         out.println("<br><br>");
-
+        
         out.println("<form name=\"logout\" action=index.html>");
         out.println("<input type=submit name=\"home\" value=\"Return to Main Menu\">");
         out.println("</form>");
-
+        
         out.println("<br>");
     }
-
-
+    
+    
     private void drawFailOptionsIncorrectAccountName(HttpServletRequest req, PrintWriter out)
     {
         out.println("<font size=5 face=\"Arial,Helvetica\">");
         out.println("<b>Error: enter the correct account name.</b></br>");
-
+        
         out.println("<hr");
         out.println("<br><br>");
-
+        
         out.println("<form name=\"logout\" action=index.html>");
         out.println("<input type=submit name=\"home\" value=\"Return to Main Menu\">");
         out.println("</form>");
-
+        
         out.println("<br>");
     }
-
-    private void drawFailOptions(HttpServletRequest req, PrintWriter out)
-    {
-        out.println("<font size=5 face=\"Arial,Helvetica\">");
-        out.println("<b>Error: account number does not exist.</b></br>");
-
-        out.println("<hr");
-        out.println("<br><br>");
-
-        out.println("<form name=\"logout\" action=index.html>");
-        out.println("<input type=submit name=\"home\" value=\"Return to Main Menu\">");
-        out.println("</form>");
-
-        out.println("<br>");
-    }
-
+    
     public void drawLoginSuccess(HttpServletRequest req, PrintWriter out)
     {
         drawHeader(req,out);
         drawActiveOptions(req,out);
         drawFooter(req,out);
     }
-
+    
     public void drawLoginFailOnNumber(HttpServletRequest req, PrintWriter out)
     {
         drawHeader(req,out);
@@ -189,19 +161,26 @@ public class LoginServlet extends HttpServlet
     	drawFailOptionsIncorrectAccountName(req,out);
     	drawFooter(req,out);
     }
-
+    
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
     {
     	System.out.println("HW8: Doing get for LoginServlet.");
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         
+        // Is the user already logged in?
+    	if(req.getSession().getAttribute("Username") != null) {
+    		drawLoginSuccess(req, out);
+    		return;
+    	}
+    	
         int accountNumber = 0;
         String ownerName = "";
-
+        
         try{
             String[] params = req.getParameterValues("account number");
         	accountNumber = Integer.parseInt(params[0]);
+        	
         } catch (Exception e) {
         	e.printStackTrace();
         }
@@ -209,27 +188,22 @@ public class LoginServlet extends HttpServlet
         try{
             String[] params = req.getParameterValues("account");
             ownerName = params[0];
+        	
         } catch (Exception e) {
         	e.printStackTrace();
         }
         
         try{
-        	// Is the user already logged in?
-        	if(req.getSession().getAttribute("Username") != null) {
-        		drawLoginSuccess(req, out);
-        		return;
-        	}
-        	
         	ResultSet rs = s.executeQuery("SELECT * FROM Account WHERE " +
-        			"AccountNumber = " + accountNumber);
+                                          "AccountNumber = " + accountNumber);
         	if(!rs.next()) {
         		drawLoginFailOnNumber(req, out);
         		return;
         	}
         	
         	rs = s.executeQuery("SELECT * FROM Account WHERE " +
-        			"AccountNumber = " + accountNumber + " AND " +
-        			"Name = \'" + ownerName + "\'");
+                                "AccountNumber = " + accountNumber + " AND " +
+                                "Name = \'" + ownerName + "\'");
         	
         	if(rs.next()) {
         		req.getSession().setAttribute("Username", ownerName);
@@ -242,10 +216,8 @@ public class LoginServlet extends HttpServlet
         catch (Exception e) {
         	e.printStackTrace();
         }
-
-        //if fail, call the following function
-        //drawLoginFail(req,out);
     }
 }
+
 
 
