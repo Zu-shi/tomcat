9d007797551a7c2a8f39193594780a37f3a2a7ae
Zuoming Shi <zuomingshi@oxford.(none)>
2014-12-10 10:57:09 -0700

    Push cahnges to web.xml

---
+++ SonoranCellular/Readme.txt
@@ -0,0 +1,10 @@
+Design considerations:
+Since the “add a plan” page does not specify for new phones or plans to be added, we assume that those phones already exist in the database, and the fields aid the user in filling out the form.
+We assume that “billing period” means a day, rather than a stretch of time.
+
+Extra features:
+Plans are fetched in a drop-down menu.
+If there are no existing plans, ‘add a plan’ page shows a warning message.
+The total for a billing period is printed in the “find-bills” page.
+Accounts can register for dependents, but only if no one else is dependent on them.
+Page for printing dependents.
--- SonoranCellular/sql/create.sql
+++ SonoranCellular/sql/create.sql
@@ -1,29 +1,29 @@
 CREATE TABLE Account(
-	AccountNumber NUMBER(10),
+	AccountNumber NUMBER(10) NOT NULL,
 	Name VARCHAR(40) NOT NULL,
 	PRIMARY KEY (AccountNumber)
 	);
 
 CREATE TABLE Phone(
-	IMEI NUMBER(10),
-	MobileNumber VARCHAR(14),
+	IMEI NUMBER(10) NOT NULL,
+	MobileNumber VARCHAR(14) NOT NULL,
 	Manufacturer VARCHAR(40) NOT NULL,
 	Model VARCHAR(40) NOT NULL,
 	PRIMARY KEY (IMEI)
 	);
 
 CREATE TABLE Plan(
-	PlanName VARCHAR(40),
-	PricePerMonth NUMERIC(10, 2) NOT NULL,
-	AllowedDataUsage NUMBER(10) NOT NULL,
+	PlanName VARCHAR(40) NOT NULL,
+	PricePerMonth NUMERIC(8, 2) NOT NULL,
+	AllowedDataUsage NUMBER(8) NOT NULL,
 	Type VARCHAR(40) NOT NULL,
 	PRIMARY KEY (PlanName)
 	);
 
 CREATE TABLE Subscribe(
-	IMEI NUMBER(10),
-	AccountNumber NUMBER(10) NOT NULL,
-	PlanName VARCHAR(40),
+	IMEI NUMBER(8),
+	AccountNumber NUMBER(8) NOT NULL,
+	PlanName VARCHAR(40) NOT NULL,
 	PRIMARY KEY (IMEI),
 	FOREIGN KEY (IMEI) REFERENCES Phone,
 	FOREIGN KEY (PlanName) REFERENCES Plan,
@@ -31,15 +31,15 @@ CREATE TABLE Subscribe(
 	);
 
 CREATE TABLE Owns(
-	MasterAccountNumber NUMBER(10),
-	DependentAccountNumber NUMBER(10),
+	MasterAccountNumber NUMBER(8) NOT NULL,
+	DependentAccountNumber NUMBER(8) NOT NULL,
 	PRIMARY KEY (DependentAccountNumber),
 	FOREIGN KEY (DependentAccountNumber) REFERENCES Account,
 	FOREIGN KEY (MasterAccountNumber) REFERENCES Account
 );
 
 CREATE TABLE Bill(
-	AccountNumber NUMBER(10),
+	AccountNumber NUMBER(8) NOT NULL,
 	EndDate DATE,
 	StartDate DATE,
 	DueDate DATE,
@@ -48,10 +48,10 @@ CREATE TABLE Bill(
 );
 
 CREATE TABLE Item(
-	AccountNumber NUMBER(10),
+	AccountNumber NUMBER(8) NOT NULL,
 	EndDate DATE,
-	ItemNumber NUMBER(10),
-	Amount NUMERIC(10,2),
+	ItemNumber NUMBER(8) NOT NULL,
+	Amount NUMERIC(8,2) NOT NULL,
 	PRIMARY KEY (AccountNumber, EndDate, ItemNumber),
 	FOREIGN KEY (AccountNumber, EndDate) REFERENCES Bill
 );
